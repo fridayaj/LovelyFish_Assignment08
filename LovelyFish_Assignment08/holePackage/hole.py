1e43abd8265fcfd3bce2c70dbed4e60c4d4d7a6c
Kyle Hsu <hsukt@mail.uc.edu>
2024-10-30 21:21:28 -0400

    Updated hole.py

--- LovelyFish_Assignment08/holePackage/hole.py
+++ LovelyFish_Assignment08/holePackage/hole.py
@@ -1,3 +1,14 @@
+# Name: Kyle Hsu
+# email: hsukt@mail.uc.edu
+# Assignment Number: Assignment 08
+# Due Date:   10/31/24
+# Course #/Section:   IS 4010 001
+# Semester/Year:   Fall 2024
+# Brief Description of the assignment:  Collaborate to model something
+
+# Brief Description of what this module does. one of the classes, updates the distance from the hole.
+# Citations: w3 schools, stackoverflow
+# Anything else that's relevant:
 class Hole:
     def __init__(self, par, distance):
         self.par = par
